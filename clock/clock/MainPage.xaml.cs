85e06b2670bb6b892374dc326384a01fcc668510
mary1 <m117kovl@mail.ru>
2021-10-21 23:27:01 +0500

    готовый проект RoundClock

---
+++ clock/clock/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Timers;
+using Xamarin.Forms;
+
+namespace clock
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var timeNow = DateTime.Now;
+            SecondHand.RotateTo(360 / 3600 * timeNow.Second);
+            MinuteHand.RotateTo(360 / 60 * timeNow.Minute);
+            HourHand.RotateTo(360 / 12 * timeNow.Hour);
+
+            InitTimer();
+        }
+
+        private void InitTimer()
+        {
+            var timer = new Timer(100);
+            timer.Elapsed += Update;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private void Update(object sender, ElapsedEventArgs e)
+        {
+            var timeNow = DateTime.Now;
+            SecondHand.Rotation = ((360 * timeNow.Second) / 60);
+            MinuteHand.Rotation = ((360 * timeNow.Minute ) / 60);
+            HourHand.Rotation = ((360 * timeNow.Hour ) / 12);
+        }
+    }
+}
